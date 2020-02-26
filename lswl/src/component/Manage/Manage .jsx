fc4ea31f42b269fc210ce6c1de91ef99cedb1275
dingruiyu123 <60870527+dingruiyu123@users.noreply.github.com>
2020-02-27 00:55:24 +0800

    添加页面布局
    添加页面布局

--- lswl/src/component/Manage/Manage .jsx
+++ lswl/src/component/Manage/Manage .jsx
@@ -14,8 +14,62 @@ render(){
                    <li className="lis"><NavLink to="/manage/song">送礼</NavLink></li>
                 </ul>
             </div>
-            <div>
-                <router-view></router-view>
+            <ul className="list2">
+                <li className="one">类别</li>
+                <li className="one">时间</li>
+                <li className="one">金额</li>
+            </ul>
+            <div className="bottom">
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
             </div>
         </div>
     )
