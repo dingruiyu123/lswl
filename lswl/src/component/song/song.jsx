4d8a80617b4182525024d6819eae70b62a77c0ce
dingruiyu123 <60870527+dingruiyu123@users.noreply.github.com>
2020-02-28 09:34:03 +0800

    修改布局
    修改布局

--- lswl/src/component/song/song.jsx
+++ lswl/src/component/song/song.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import './song.css'
 export default class Song extends Component{
 constructor(){
     super()
@@ -6,7 +7,63 @@ constructor(){
 render(){
     return(
         <div>
-         cccvv
+            <ul className="lie">
+                <li className="one">类别</li>
+                <li className="one">时间</li>
+                <li className="one">金额</li>
+            </ul>
+            <div className="butt">
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+                <div className="two">
+                    <p className="ming">
+                      <span className="lei">类目名称</span>
+                      <span className="red">15000.00</span>
+                    </p>
+                    <p className="ming">
+                       <span className="xing">姓名</span>
+                       <span className="shi">10人送礼  2016.5.21</span>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
