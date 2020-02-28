a6db4d5d18748f8b4e2db7b8d362ff4c97b354ad
dingruiyu123 <60870527+dingruiyu123@users.noreply.github.com>
2020-02-28 09:45:37 +0800

    修改布局
    修改版布局

--- lswl/src/component/dai/dai.jsx
+++ lswl/src/component/dai/dai.jsx
@@ -6,7 +6,42 @@ constructor(){
 render(){
     return(
         <div>
-         
+            <ul className="dcj">
+                <li className="three">类别</li>
+                <li className="three">时间</li>
+            </ul>
+            <div className="fot">
+                <div className="bott">
+                    <p className="ditu">
+                      <span className="da">王大锤搬家请客</span>
+                      <span className="dai">带参加</span>
+                    </p>
+                    <p className="ditu">
+                      <span className="di">地点名称地点名称</span>
+                      <span className="qi">2016.8.21</span>
+                   </p>
+                </div>
+                <div className="bott">
+                    <p className="ditu">
+                      <span className="da">王大锤搬家请客</span>
+                      <span className="dai">已参加</span>
+                    </p>
+                    <p className="ditu">
+                      <span className="di">地点名称地点名称</span>
+                      <span className="qi">2016.6.21</span>
+                   </p>
+                </div>
+                <div className="bott">
+                    <p className="ditu">
+                      <span className="da">王大锤搬家请客</span>
+                      <span className="dai">带参加</span>
+                    </p>
+                    <p className="ditu">
+                      <span className="di">地点名称地点名称</span>
+                      <span className="qi">2016.5.21</span>
+                   </p>
+                </div>
+            </div>
         </div>
     )
 }
