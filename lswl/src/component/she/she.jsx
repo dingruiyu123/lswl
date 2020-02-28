52c1de158d4eb8de249f43a5d7a11dc73447e1d3
dingruiyu123 <60870527+dingruiyu123@users.noreply.github.com>
2020-02-28 09:54:21 +0800

    项目
    项目

---
+++ lswl/src/component/she/she.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+export default class She extends Component{
+constructor(){
+    super()
+}
+render(){
+    return(
+        <div>
+        
+        </div>
+    )
+}
+}
